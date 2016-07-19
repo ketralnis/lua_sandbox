4b339bbb3656fbd009bc094d9d8069a7ce77e645
David King <dking@ketralnis.com>
2016-07-19 11:28:22 -0700

    Change the default include_dirs to look like ubuntu's

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ else:
     sys.stderr.write("couldn't find %s\n" % (BUILDCONF,))
 
 LUA_LIB = build_config.get('lua_lib', 'lua5.2')
-INCLUDE_DIRS = build_config.get('include_dirs', [])
+INCLUDE_DIRS = build_config.get('include_dirs', ["/usr/include/lua5.2"])
 LIBRARY_DIRS = build_config.get('library_dirs', [])
 EXTRA_COMPILE_ARGS = build_config.get('extra_compile_args', [])
 EXTRA_LINK_ARGS = build_config.get('extra_link_args', [])

f7c6789988fe4bdcc2e8fbe5dcb62228825e77fe
David King <dking@ketralnis.com>
2016-09-18 21:10:45 -0700

    add vtest -p shortcut

--- vtest.sh
+++ vtest.sh
@@ -10,6 +10,8 @@ if [ "$1" = "-d" ]; then
     vrun 'gdb $(which python) --args python -m lua_sandbox.tests.tests'
 elif [ "$1" = "-l" ]; then
     vrun 'LEAKTEST=1 python -m lua_sandbox.tests.tests'
+elif [ "$1" = "-p" ]; then
+    vrun 'python -m lua_sandbox.tests.perf'
 else
     vrun 'python -m lua_sandbox.tests.tests'
 fi
